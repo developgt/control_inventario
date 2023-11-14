2f51e0bdc5659a99e21db7bee082f665083ceee1
Nozen <132400977+ramirezedelvalle@users.noreply.github.com>
2023-11-14 15:41:21 -0600

    creacion de paginas interactivas

--- public/index.php
+++ public/index.php
@@ -8,12 +8,18 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 use Controllers\AppController;
 use Controllers\MenuController;
+use Controllers\GestionController;
 
 $router = new Router();
 $router->setBaseURL('/' . $_ENV['APP_NAME']);
 
 $router->get('/', [MenuController::class,'index']);
 $router->get('/API/menu/buscar', [MenuController::class,'buscarAlmacen']);
+
+$router->get('/gestion', [GestionController::class,'index']);
+$router->get('/API/gestion/buscar', [GestionController::class,'buscarAlmacen']);
+
+$router->get('/guardalmacen', [GuardalmacenController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
