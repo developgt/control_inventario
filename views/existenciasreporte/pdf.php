3a1fc8c3ee02542618c756a1774237eeec211e81
marino5710 <brianmarin4@gmail.com>
2023-11-28 17:30:22 -0600

    se finalizo de crear

--- views/existenciasreporte/pdf.php
+++ views/existenciasreporte/pdf.php
@@ -5,25 +5,34 @@
     <meta charset="UTF-8">
     <title>Reporte de Insumos</title>
     <style>
-        body {
+         body {
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
+            background-color: #f4f4f4;
         }
 
         .report-header {
             text-align: center;
             margin-top: 20px;
+            padding-bottom: 10px;
         }
 
-        .report-details {
+        .report-footer {
+            border-top: 5px solid #007bff; /* Línea azul en el pie de página */
+            padding-top: 10px;
+            color: #6c757d;
             text-align: center;
-            margin-top: 5px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            background-color: #e3f2fd; /* Fondo celeste metálico en el pie de página */
         }
 
         .report-title {
             font-size: 24px;
             margin: 20px 0;
+            color: #000; /* Título en negro */
         }
 
         .styled-table {
@@ -31,16 +40,13 @@
             border-collapse: collapse;
             font-size: 0.9em;
             margin-bottom: 30px;
-        }
-
-        .report-title {
-            font-size: 24px;
-            margin: 20px 0;
-            color: #333;
+            background-color: #fff;
+            border: 2px solid #000; /* Bordes más definidos para la tabla */
         }
 
         .styled-table th {
-            background-color: #f2f2f2;
+            background-color: #e3f2fd; /* Encabezados en celeste metálico */
+            color: #000; /* Texto oscuro para contraste */
         }
 
         .styled-table td {
@@ -50,13 +56,17 @@
         .styled-table th,
         .styled-table td {
             padding: 12px 15px;
-            border: 1px solid #050505;
+            border: 1px solid #000; /* Bordes negros para cada celda */
         }
 
+        .styled-table tbody tr:nth-child(even) {
+            background-color: #f2f2f2; /* Filas alternas en gris claro */
+        }
 
         .fecha {
-            float: left;
-            width: 40%;
+            margin-top: 5px;
+            color: #6c757d;
+            font-size: 0.8em;
         }
 
   
@@ -64,6 +74,17 @@
 </head>
 
 <body>
+    <header>
+  
+    <div style="position: relative; text-align: center;">
+    <span>Ejército de Guatemala</span>
+        <div style="position: absolute; top: -50px; left: 50%; transform: translateX(-50%);">
+            <img src="./images/min.png" alt="Descripción de la imagen" style="width: 80px; height: auto;">
+        </div>
+    </div>
+
+
+    </header>
     <div class="report-header">
         <h1 class="report-title">Reporte de Existencias de Insumos del inventario "<?= htmlspecialchars($datos[0]->alma_nombre) ?>"</h1>
         <div class="fecha">
@@ -97,7 +118,12 @@
                 </tr>
             <?php endforeach; ?>
         </tbody>
-
     </table>
+
+
+
+    <div class="report-footer">
+        <span>Sistema de Gestión de Control de Inventarios - Reporte generado automáticamente</span>
+    </div>
 </body>
 </html>
