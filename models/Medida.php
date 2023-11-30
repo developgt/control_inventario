0c803c7d54e948ac859e6c1ef046d28afd556bf9
marino5710 <brianmarin4@gmail.com>
2023-11-30 09:12:24 -0600

    se eliminaron los archivos de producto y de medida, debido a que por instrucciones recibidas estos se asignaran desde la base de datos... a la clase correspondiente

--- models/Medida.php
+++ models/Medida.php
@@ -5,12 +5,12 @@ namespace Model;
 class Medida extends ActiveRecord
 {
     protected static $tabla = 'inv_uni_med';
-    protected static $columnasDB = ['uni_nombre', 'uni_almacen', 'uni_situacion'];
+    protected static $columnasDB = ['uni_nombre', 'uni_clase', 'uni_situacion'];
     protected static $idTabla = 'uni_id';
 
     public $uni_id;
     public $uni_nombre;
-    public $uni_almacen;
+    public $uni_clase;
     public $uni_situacion;
    
 
@@ -18,7 +18,7 @@ class Medida extends ActiveRecord
     {
         $this->uni_id = $args['uni_id'] ?? null;
         $this->uni_nombre = $args['uni_nombre'] ?? '';
-        $this->uni_almacen = $args['uni_almacen'] ?? '';
+        $this->uni_clase = $args['uni_clase'] ?? '';
         $this->uni_situacion = $args['uni_situacion'] ?? 1;
     }
 }
--- models/Producto.php
+++ models/Producto.php
@@ -5,12 +5,11 @@ namespace Model;
 class Producto extends ActiveRecord
 {
     protected static $tabla = 'inv_producto';
-    protected static $columnasDB = ['pro_almacen_id', 'pro_medida', 'pro_nom_articulo', 'pro_descripcion', 'pro_situacion'];
+    protected static $columnasDB = ['pro_clase_id',  'pro_nom_articulo', 'pro_descripcion', 'pro_situacion'];
     protected static $idTabla = 'pro_id';
 
     public $pro_id;
-    public $pro_almacen_id;
-    public $pro_medida;
+    public $pro_clase_id;
     public $pro_nom_articulo;
     public $pro_descripcion;
     public $pro_situacion;
@@ -20,8 +19,7 @@ class Producto extends ActiveRecord
     public function __construct($args = [])
     {
         $this->pro_id = $args['pro_id'] ?? null;
-        $this->pro_almacen_id = $args['pro_almacen_id'] ?? '';
-        $this->pro_medida = $args['pro_medida'] ?? '';
+        $this->pro_clase_id = $args['pro_clase_id'] ?? '';
         $this->pro_nom_articulo = $args['pro_nom_articulo'] ?? '';
         $this->pro_descripcion = $args['pro_descripcion'] ?? '';
         $this->pro_situacion = $args['pro_situacion'] ?? 1;
